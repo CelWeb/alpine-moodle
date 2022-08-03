2566caf0593d484252e777c3df0ae729f3948cd8
Artur <38910233+CelWeb@users.noreply.github.com>
2022-08-03 10:30:22 +0300

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -3,7 +3,7 @@ apk --no-cache add curl
 
 # Check that the database is available
 echo "Waiting for moodle to be ready"
-while ! nc -w 1 app 8080; do
+while ! nc -w 1 app 8085; do
     # Show some progress
     echo -n '.';
     sleep 1;
@@ -12,4 +12,4 @@ echo "moodle is ready"
 # Give it another 3 seconds.
 sleep 3;
 
-curl --silent --fail http://app:8080 | grep '<title>Dockerized_Moodle</title>'
+curl --silent --fail http://app:8085 | grep '<title>Dockerized_Moodle</title>'
